b4b65d84f7a01e11af92a70bdc4ec4f6529723ac
aymaneMx <aymanemx@gmail.com>
2020-08-01 16:19:54 +0100

    :rocket: Set up Elastic-search

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_elasticsearch_dsl',
 ]
 
 MIDDLEWARE = [
@@ -109,3 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Elasticsearch
+ELASTICSEARCH_DSL = {
+    'default': {
+        'hosts': 'es:9200'  # our host using Docker
+    },
+}
